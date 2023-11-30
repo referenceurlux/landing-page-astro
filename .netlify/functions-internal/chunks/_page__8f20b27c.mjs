d4922e97ab98dfea11a37bcec4ddcedde1af2514
Beda <steve@referenceur.co>
2023-11-30 16:28:38 +0100

    fix path

---
+++ .netlify/functions-internal/chunks/_page__8f20b27c.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/_page__fbe648a4.mjs').then(n => n._);
+
+export { page };
