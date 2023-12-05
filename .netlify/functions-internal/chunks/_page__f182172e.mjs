66da1a9e75e722c28808975f2723711c7da991ab
Beda <steve@referenceur.co>
2023-12-05 13:54:58 +0100

    test mailgun

---
+++ .netlify/functions-internal/chunks/_page__f182172e.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/_page__25c03f1c.mjs').then(n => n._);
+
+export { page };
---
+++ .netlify/functions-internal/chunks/index_18e94944.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_aa6f82b1.mjs');
+
+export { page };
