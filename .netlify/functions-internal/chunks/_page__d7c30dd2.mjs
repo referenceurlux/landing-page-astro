31a375d1e43c48cc71dd13a0fb0c335c211856d0
Beda <steve@referenceur.co>
2023-12-05 14:31:18 +0100

    fix netlify

---
+++ .netlify/functions-internal/chunks/_page__d7c30dd2.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/_page__fa2b9854.mjs').then(n => n._);
+
+export { page };
