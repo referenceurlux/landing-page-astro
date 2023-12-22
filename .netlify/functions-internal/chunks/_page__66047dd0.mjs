40cf1ec4fe4347ac0b3a7aa551a7f7d3c7c3c936
Beda <steve@referenceur.co>
2023-12-22 09:44:04 +0100

    css recaptcha

---
+++ .netlify/functions-internal/chunks/_page__66047dd0.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/_page__c217ed61.mjs').then(n => n._);
+
+export { page };
---
+++ .netlify/functions-internal/chunks/index_6a492192.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_30469b86.mjs');
+
+export { page };
