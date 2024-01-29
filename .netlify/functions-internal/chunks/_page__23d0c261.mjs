e2640fe8cd295ae1b3b54ca5793e3a085e78500b
Beda <steve@referenceur.co>
2024-01-29 15:45:58 +0100

    push file submit

---
+++ .netlify/functions-internal/chunks/_page__23d0c261.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/_page__f40694dd.mjs').then(n => n._);
+
+export { page };
