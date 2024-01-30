a348ff494945df077a6ccc97d09124eb1ffb5103
Beda <steve@referenceur.co>
2024-01-30 11:13:43 +0100

    test netlify form

---
+++ .netlify/functions-internal/chunks/_page__61cd0afc.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/_page__68a5fcfe.mjs').then(n => n._);
+
+export { page };
