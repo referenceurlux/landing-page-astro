d9598eed79fdad93435de5a3f26670ffa22f98b3
Beda <steve@referenceur.co>
2024-02-21 15:59:28 +0100

    fix gallery

---
+++ .netlify/functions-internal/chunks/_page__06a22a4c.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/_page__f60e7b37.mjs').then(n => n._);
+
+export { page };
---
+++ .netlify/functions-internal/chunks/index_eae10df2.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_af52bd0e.mjs');
+
+export { page };
