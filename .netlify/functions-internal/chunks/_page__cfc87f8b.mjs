1759ee59a40d4a36b9969efc57f8b8115f499660
Beda <steve@referenceur.co>
2024-02-21 16:16:29 +0100

    fix bug

---
+++ .netlify/functions-internal/chunks/_page__cfc87f8b.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/_page__23be5d9e.mjs').then(n => n._);
+
+export { page };
---
+++ .netlify/functions-internal/chunks/index_3971be4e.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_009c9d88.mjs');
+
+export { page };
