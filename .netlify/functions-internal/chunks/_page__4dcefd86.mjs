25b595813d817a2ec6df94a7266ab8779eae92a1
Beda <steve@referenceur.co>
2024-04-15 14:48:58 +0200

    update header

---
+++ .netlify/functions-internal/chunks/_page__4dcefd86.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/_page__dafa46f0.mjs').then(n => n._);
+
+export { page };
---
+++ .netlify/functions-internal/chunks/index_43ca2ab8.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_9547b0c3.mjs');
+
+export { page };
